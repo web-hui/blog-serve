34cdd56c115ae57c990541af24bf8fceb51d8edc
web-hui <738905105@qq.com>
2020-08-09 15:57:01 +0800

    feat: :sparkles: 增加权限功能，签发token
    登陆签发token，后续请求需要验证token

---
+++ src/modules/auth/auth.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@nestjs/common';
+import { UsersService } from '../users/users.service';
+import { JwtService } from '@nestjs/jwt';
+
+@Injectable()
+export class AuthService {
+  constructor(
+    private readonly usersService: UsersService,
+    private readonly jwtService: JwtService,
+  ) {}
+
+  async validateUser(username: string, pass: string): Promise<any> {
+    const user = await this.usersService.byUserName(username);
+    if (user && user.password === pass) {
+      // eslint-disable-next-line @typescript-eslint/no-unused-vars
+      const { password, ...result } = user;
+      return result;
+    }
+    return null;
+  }
+
+  async login(user: {
+    username: string;
+    id?: string;
+  }): Promise<{ token: string }> {
+    const payload = { username: user.username, id: user.id };
+    return {
+      token: this.jwtService.sign(payload),
+    };
+  }
+}
